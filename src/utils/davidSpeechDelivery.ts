1a1f5adbf22472a41102285c9d52f2c4763ce2da
MellYell22 <missliss22@icloud.com>
2026-06-05 09:22:53 -0400

    Repair and calm David speech delivery

--- src/utils/davidSpeechDelivery.ts
+++ src/utils/davidSpeechDelivery.ts
@@ -13,7 +13,7 @@ export type HumanizeOptions = {
 const TRAILING_PAUSE_MARKS = /[\s,;:-]+$/;
 
 const SOFT_FILLER_RE =
-  /^(mm+|hmm+|hm+|yeah|hey|okay|alright|you know|i mean|well)[,.…\s]+/i;
+  /^(mm+|hmm+|hm+|yeah|hey|okay|alright|you know|i mean|well)[,.\.\s]+/i;
 
 const SCRIPTED_MARKUP_RE =
-  /\[(?:soft\s+breath|breath|inhale|exhale|sigh|pause)\]|\((?:soft\s+breath|breath|inhale|exhale|sigh|pause)\)|\*(?:soft\s+breath|breath|inhale|exhale
+  /\[(?:soft\
